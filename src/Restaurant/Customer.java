465a08d61a48d97debf5e20a9eb06d854130a684
ParkerYang1 <parker.yang23@ycdsbk12.ca>
2022-11-10 10:29:54 -0500

    Constructor for customer

--- src/Restaurant/Customer.java
+++ src/Restaurant/Customer.java
@@ -1,4 +1,27 @@
 package Restaurant;
-public class Customer {
-    
+
+/**
+ * OOP Assignment customer class
+ * @author: Parker Yang
+ * 
+ */
+
+public class Customer extends Human{
+    //Instance variables 
+    private String address;
+    private int customers; 
+
+    /**
+     * Constructor for customer
+     * @param fName First name
+     * @param lName Last name
+     * @param customerAddress Address 
+     * @param totalCustomers Total # of customers in the restaurant
+     * 
+     */
+    public Customer(String fName, String lName, String customerAddress, int totalCustomers){
+        super(fName, lName);
+        this.address = customerAddress; 
+        this.customers = totalCustomers;
+    }
 }
