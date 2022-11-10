4fb1ba90734542ec95998bd94e0f96785a0782ce
ParkerYang1 <parker.yang23@ycdsbk12.ca>
2022-11-09 22:16:02 -0500

    Constructor for human

--- src/Restaurant/Human.java
+++ src/Restaurant/Human.java
@@ -1,5 +1,26 @@
 package Restaurant;
 
+/**
+* OOP Assignment Human Superclass
+* @author: Parker Yang
+* 
+*/
+
 public class Human {
+    // Instance variables 
+    private String firstName;
+    private String lastName; 
+
+    /**
+     * Constructor for human
+     * @param fName first name
+     * @param lName last name
+     * @author: Parker Yang
+     */
+    public Human(String fName, String lName){
+        firstName = fName;
+        lastName = lName;
+    }
+
     
 }
