e7d784ac8497bbe01868ca83d2eca6a48a2af084
ParkerYang1 <parker.yang23@ycdsbk12.ca>
2022-11-09 23:28:53 -0500

    toString method + edit java docs

--- src/Restaurant/Chef.java
+++ src/Restaurant/Chef.java
@@ -7,14 +7,16 @@ package Restaurant;
 */
 
 public class Chef extends Human{
+    // Instance variables 
     private int salary; 
 
     /**
      * Constructor for Chef
-     * @param fName first name
-     * @param lName last name
-     * @param jobSalary salary for chef
+     * @param fName First name
+     * @param lName Last name
+     * @param jobSalary Salary for chef
      * @author: Parker Yang
+     * 
      */
     public Chef(String fName, String lName, int jobSalary){
         super(fName, lName);
@@ -23,10 +25,22 @@ public class Chef extends Human{
 
     /**
      * Getter method for salary 
-     * @return salary 
+     * @return Salary 
      * @author: Parker Yang
+     * 
      */
     public int getSalary(){
         return salary;
     }
+
+    /**
+     * Outputs sentence with information about the chef
+     * @return Introduction of chef
+     * @author: Parker Yang
+     * 
+     */
+    public String toString(){
+        return "Hello, my name is" + this.getFirstName() + " " + this.getLastName() + " and I will be your chef for today. " + "(Salary: " + salary + ")"; 
+    }
+
 }
