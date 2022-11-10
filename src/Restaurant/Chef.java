692bc8a0daa31e2a8f29adf039dc1aeeaafae1d8
ParkerYang1 <parker.yang23@ycdsbk12.ca>
2022-11-09 23:14:56 -0500

    Constructor for Chef

--- src/Restaurant/Chef.java
+++ src/Restaurant/Chef.java
@@ -1,4 +1,23 @@
 package Restaurant;
-public class Chef {
-    
+
+/**
+* OOP Assignment Chef class
+* @author: Parker Yang
+* 
+*/
+
+public class Chef extends Human{
+    private int salary; 
+
+    /**
+     * Constructor for Chef
+     * @param fName first name
+     * @param lName last name
+     * @param jobSalary salary for chef
+     * @author: Parker Yang
+     */
+    public Chef(String fName, String lName, int jobSalary){
+        super(fName, lName);
+        this.salary = jobSalary;
+    }
 }
