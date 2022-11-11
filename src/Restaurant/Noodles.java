a56304fecfe3798963f6b428d7380f83b49c7042
ParkerYang1 <parker.yang23@ycdsbk12.ca>
2022-11-11 10:31:26 -0500

    Constructor for noodles

--- src/Restaurant/Noodles.java
+++ src/Restaurant/Noodles.java
@@ -1,4 +1,24 @@
 package Restaurant;
-public class Noodles {
-    
+
+/**
+ * OOP Assignment Noodles class
+ * @author: Parker Yang
+ * 
+ */
+public class Noodles extends Food{
+    // Instance variables
+    private String sauce;
+
+    /**
+     * Constructor for noodles 
+     * @param orderID The number code for each food
+     * @param numServings Numbers of servings for each food
+     * @param noodleSauce Sauce for the noodles 
+     * @author: Parker Yang
+     * 
+     */
+    public Noodles(int orderID, int numServings, String noodleSauce){
+        super(orderID, numServings);
+        this.sauce = noodleSauce;
+    }
 }
